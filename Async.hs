8430c475f50b1c2dad92a2a8830e6239e3344f81
rangelsen <roy.angelsen@gmail.com>
2022-06-17 15:46:13 +0200

    Create example of withAsync

--- Async.hs
+++ Async.hs
@@ -1,7 +1,16 @@
 import Control.Concurrent.Async
 import Control.Concurrent
 
-func1 = do
+importantWork :: IO Int
+importantWork = do
     putStrLn "func1 started"
-    threadDelay 10000
+    threadDelay 2000000
     putStrLn "func1 exited"
+    return 70
+
+runImportantWork :: IO ()
+runImportantWork = do
+    withAsync importantWork $ \i -> do
+        asyncRes <- wait i
+        putStrLn $ "Important work result: " ++ show asyncRes
+    putStrLn "Exiting..."
